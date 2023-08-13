4810fd25c5aec07e28aee68188e0324d0e4cffb0
Quincy Maynor <quincymaynor@gmail.com>
2023-08-13 10:45:24 -0500

    updated comment routes

--- Main/controllers/api/commentRoutes.js
+++ Main/controllers/api/commentRoutes.js
@@ -2,37 +2,78 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// get all comments
+router.get('/', async (req, res) => {
+  try {
+      const commentData = await Comment.findAll();
+      res.status(200).json(commentData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
+// read single comment 
+router.get('/:id', async (req, res) => {
+  try {
+      const commentData = await Comment.findByPk(req.params.id);
+
+      if (!commentData) {
+          res.status(404).json({ message: 'No comment found with that id!' });
+          return;
+      }
+
+      res.status(200).json(commentData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
+// create comment
+router.post('/', async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
-    res.status(200).json(newComment);
+      res.status(200).json(newComment);
   } catch (err) {
-    res.status(400).json(err);
+      res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+// update comment
+router.put('/:id', async (req, res) => {
   try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
+      const commentData = await Comment.findByPk(req.params.id);
+      commentData.set({
+        ...req.body,
         user_id: req.session.user_id,
-      },
-    });
-
-    if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
-      return;
-    }
-
-    res.status(200).json(commentData);
+      });
+      await commentData.save();
+      res.status(200).json(commentData);
   } catch (err) {
-    res.status(500).json(err);
+      res.status(400).json(err);
   }
+})
+
+// delete comment
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with that id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- Main/models/index.js
+++ Main/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./users');
 const Post = require('./posts');
+const Comment = require('./comments');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -10,4 +11,31 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+Comment.hasMany(Comment, {
+  foreignKey: "parent_comment_id",
+  as: 'child_comment'
+});
+
+Comment.belongsTo(Comment, {
+  foreignKey: "parent_comment_id",
+  as: 'parent_comment'
+});
+
+
+module.exports = { User, Post, Comment };
