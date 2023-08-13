330bca8a52441eba90fd868a8c174560c42f34c7
Quincy Maynor <quincymaynor@gmail.com>
2023-08-13 17:35:48 -0500

    added helper functions

--- Main/utils/helpers.js
+++ Main/utils/helpers.js
@@ -1,22 +1,31 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
+    // date format MM/DD/YYYY
     return date.toLocaleDateString();
   },
   format_amount: (amount) => {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
-  get_emoji: () => {
-    const randomNum = Math.random();
-
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
+  ifEq: (arg1, arg2, options) => {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
   },
+  ifNEq: (arg1, arg2, options) => {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+  },
+  build_comment_tree: (comments, logged_in_user) => {
+    const buildTree = (comments, parent_id = null, level = 2) =>
+      comments.filter(comment => comment.parent_comment_id === parent_id)
+        .map((child) => `<div class="pl-${level}">
+          <details>
+          <summary>${child.user.name}: ${child.comment} ${child.user.id === logged_in_user ?
+            `<button class="btn btn-error delete-comment" id="${child.id}">delete</button>` :
+            `<button class="btn btn-neutral add-comment" id="${child.id}">reply</button>`}
+          </summary>
+          ${buildTree(comments, child.id, level + 2)}
+          </details>
+        </div>`)
+
+    return buildTree(comments).join("");
+  }
 };
