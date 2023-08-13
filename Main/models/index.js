91c37a96e1c5052ab089692bba85c1ea2b734275
Quincy Maynor <quincymaynor@gmail.com>
2023-08-13 10:32:17 -0500

    added comment model, updated post model and home routes

---
+++ Main/models/index.js
@@ -0,0 +1,13 @@
+const User = require('./users');
+const Post = require('./posts');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
